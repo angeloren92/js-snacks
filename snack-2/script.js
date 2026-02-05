2d69c9a61be00d4419268c017536b4d67af7fb69
angeloren92 <angeloren92@gmail.com>
2026-02-05 11:32:43 +0100

    correction snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,6 +1,5 @@
 console.log('test');
 
-//utente inserisce 2 parole, il prompt stampa prima la parola piu corta e poi la piu lunga
 
 const parola1 = prompt('inserisci una parola: ');
 const parola2 = prompt('inserisci altra parola: ');
@@ -9,12 +8,12 @@ const lunghezza = [parola1.length, parola2.length];
 if (isNaN(parola1 && parola2)) {
     let message;
     if (lunghezza[0] < lunghezza[1]) {
-        console.log(parola2);
+        console.log(parola1, parola2);
     } else if (lunghezza[0] > lunghezza[1]) {
-        console.log(parola1);
+        console.log(parola2, parola1);
     } else {
         message = 'le parole sono della stessa lunghezza';
-        console.log(message);
+        console.log(message, parola1, parola2);
     }
 } else {
     message = 'inserisci delle parole!!!'
