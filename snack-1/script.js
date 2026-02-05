038169bc9a0a0bd10f136e9a13d9a0bcb77324f2
angeloren92 <angeloren92@gmail.com>
2026-02-05 10:34:30 +0100

    end snack

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,2 +1,12 @@
 console.log('test');
 
+const firstNumber = parseInt(prompt('Inserisci primo numero:'));
+const secondNumber = parseInt(prompt('Inserisci secondo numero:'));
+
+if (firstNumber < secondNumber) {
+    alert('il numero maggiore è: ' + secondNumber);
+} else if (firstNumber > secondNumber) {
+    alert('il primo numero è il maggiore: ' + firstNumber);
+} else {
+    alert('I due numeri sono guauali!!');
+}
