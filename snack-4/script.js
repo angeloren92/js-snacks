3fc15d27c6eac86f7bcad3b7ff922cc5c816a157
angeloren92 <angeloren92@gmail.com>
2026-02-05 12:45:24 +0100

    complete snack3

--- snack-4/script.js
+++ snack-4/script.js
@@ -1,2 +1,11 @@
 console.log('test');
 
+let somma = 0;
+
+for (let i = 0; i < 6; i++) {
+    let numero = parseInt(prompt('Inserisci un numero: '));
+    if (numero % 2 !== 0) {
+        somma = somma + numero;
+    }
+}
+console.log(somma);
