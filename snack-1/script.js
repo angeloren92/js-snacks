e9170535eaf5087716f0afe5777524a5dc957e10
angeloren92 <angeloren92@gmail.com>
2026-02-05 10:52:48 +0100

    additional condition on blocco-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,10 +3,15 @@ console.log('test');
 const firstNumber = parseInt(prompt('Inserisci primo numero:'));
 const secondNumber = parseInt(prompt('Inserisci secondo numero:'));
 
-if (firstNumber < secondNumber) {
-    alert('il numero maggiore è: ' + secondNumber);
-} else if (firstNumber > secondNumber) {
-    alert('il primo numero è il maggiore: ' + firstNumber);
+if (!isNaN(firstNumber && secondNumber)) {
+    if (firstNumber < secondNumber) {
+        alert('il numero maggiore è: ' + secondNumber);
+    } else if (firstNumber > secondNumber) {
+        alert('il primo numero è il maggiore: ' + firstNumber);
+    } else {
+        alert('I due numeri sono guauali!!');
+    }
 } else {
-    alert('I due numeri sono guauali!!');
+    alert('Inserisci solo numeri per favore');
+
 }
